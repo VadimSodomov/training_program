415907609659cd0c7d147179470cf6d2eb1c674d
Vadim <vadim.sodomov@yandex.ru>
2024-02-01 12:50:35 +0300

    first commit

--- handlers/day1.py
+++ handlers/day1.py
@@ -4,44 +4,22 @@ import telebot
 
 from init_bot import bot
 
+def func(f, message: telebot.types.Message, n):
+    with open(f"handlers/{f}.txt", encoding="utf-8") as file:
+        s = file.read().split("$\n")
+        ex = random.choice(s)
+        image = f"handlers/{f}_img/{ex[0]}.jpeg"
+        with open(image, "rb") as photo:
+            bot.send_photo(message.chat.id, photo, caption=f"{n}. "+ex[3:])
+
 
 @bot.message_handler(commands=["day_one"])
 def get_day_one(message: telebot.types.Message):
+    group = ["grudak", "triceps"]
+    part = ["verh", "midle", "niz"]
     bot.send_message(message.chat.id, "1 часть тренировки. Проработка грудных мышц.")
-    with open("handlers/grudak/verh.txt", encoding="utf-8") as file:
-        s = file.read().split("$\n")
-        ex1 = random.choice(s)
-        image1 = f"handlers/grudak/verh_img/{ex1[0]}.jpeg"
-        with open(image1, "rb") as photo:
-            bot.send_photo(message.chat.id, photo, caption="1. "+ex1[3:])
-    with open("handlers/grudak/midle.txt", encoding="utf-8") as file:
-        s = file.read().split("$\n")
-        ex2 = random.choice(s)
-        image2 = f"handlers/grudak/midle_img/{ex2[0]}.jpeg"
-        with open(image2, "rb") as photo:
-            bot.send_photo(message.chat.id, photo, caption="2. " + ex2[3:])
-    with open("handlers/grudak/niz.txt", encoding="utf-8") as file:
-        s = file.read().split("$\n")
-        ex3 = random.choice(s)
-        image3 = f"handlers/grudak/niz_img/{ex3[0]}.jpeg"
-        with open(image3, "rb") as photo:
-            bot.send_photo(message.chat.id, photo, caption="3. " + ex3[3:])
+    for p in part:
+        func(f"{group[0]}/{p}", message, part.index(p)+1)
     bot.send_message(message.chat.id, "2 часть тренировки. Проработка трицепса.")
-    with open("handlers/triceps/verh.txt", encoding="utf-8") as file:
-        s = file.read().split("$\n")
-        ex1 = random.choice(s)
-        image1 = f"handlers/triceps/verh_img/{ex1[0]}.jpeg"
-        with open(image1, "rb") as photo:
-            bot.send_photo(message.chat.id, photo, caption="1. " + ex1[3:])
-    with open("handlers/triceps/midle.txt", encoding="utf-8") as file:
-        s = file.read().split("$\n")
-        ex2 = random.choice(s)
-        image2 = f"handlers/triceps/midle_img/{ex2[0]}.jpeg"
-        with open(image2, "rb") as photo:
-            bot.send_photo(message.chat.id, photo, caption="2. " + ex2[3:])
-    with open("handlers/triceps/niz.txt", encoding="utf-8") as file:
-        s = file.read().split("$\n")
-        ex3 = random.choice(s)
-        image3 = f"handlers/triceps/niz_img/{ex3[0]}.jpeg"
-        with open(image3, "rb") as photo:
-            bot.send_photo(message.chat.id, photo, caption="3. " + ex3[3:])
+    for p in part:
+        func(f"{group[1]}/{p}", message, part.index(p) + 1)
